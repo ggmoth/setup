c619c1ed1e64319c530362880ca41da213af5034
Mason Giebel <mmgiebel@gmail.com>
2025-08-19 22:13:23 -0500

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo pacman -S kitty
+sudo pacman -S fish
+sudo pacman -S vscode
+sudo pacman -S remmina
+sudo pacman -S discord
+code --install-extension jackpaget.gruvbox-glass
+cp -r /config/yazi ~/.config/
+cp -r /config/kitty ~/.config/
