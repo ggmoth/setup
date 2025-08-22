fb8428c999c8ae735bd8ac87cd2e9af36eb87e60
Mason Giebel <mmgiebel@gmail.com>
2025-08-22 13:16:34 -0500

    added yay to the install script

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ sudo pacman -S vscode
 sudo pacman -S remmina
 sudo pacman -S freerdp
 sudo pacman -S discord
+sudo pacman -S yay
 code --install-extension jackpaget.gruvbox-glass
 cp -r config/yazi ~/.config/
 cp -r config/kitty ~/.config/
